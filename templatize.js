6b4e0d8aeba72413454e19e4cfc38a189955b236
kartikthum <kartikthum@gmail.com>
2014-11-16 11:47:35 +0530

    Update templatize.js

--- templatize.js
+++ templatize.js
@@ -1,6 +1,6 @@
 (function(){
     function templatize(a){
-        return new Function("token","var buffer=[];buffer.push('"+a.replace(/\n/g,"\\n").replace(/'/g,"\\'").replace(/\[([^\:]*)\:([^\[]*)\[/g,"');for(var $2=0;$2<token.$1.length;$2++){buffer.push('").replace(/\]\]\.join\(([^\)]*)\)/g,"');buffer.push('$1');}buffer.pop();buffer.push('").replace(/\]\]/g,"');}buffer.push('").replace(/{{([^}]*)}}/g,"',token.$1,'")+"');return buffer.join('');");
+        return new Function("scope","var buffer=[];buffer.push('"+a.replace(/\n/g,"\\n").replace(/'/g,"\\'").replace(/\[([^\:\]]*)\:([^\[]*)\[/g,"');for(var $2=0;$2<$1.length;$2++){buffer.push('").replace(/\[\?([^\[]*)\?\[/g,"');if($1){buffer.push('").replace(/\]\]\.join\(([^\)]*)\)/g,"');buffer.push('$1');}buffer.pop();buffer.push('").replace(/\]\]/g,"');}buffer.push('").replace(/{{([^}]*)}}/g,"',$1,'").replace(/\<\!\-\-/g,"").replace(/\-\-\>/g,"")+"');return buffer.join('');");
     }
     if(typeof exports !== 'undefined' && this.exports !== exports){
         module.exports = templatize;
